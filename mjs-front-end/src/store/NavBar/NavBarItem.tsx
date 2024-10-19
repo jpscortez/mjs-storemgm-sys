eb8991bf08c186d6e1bab5a2251768a6a79c690e
joao cortez <jps.cortez@hotmail.com>
2024-10-19 11:39:15 -0300

    nav bar min style

---
+++ mjs-front-end/src/store/NavBar/NavBarItem.tsx
@@ -0,0 +1,23 @@
+import { NavLink, NavLinkProps } from "react-router-dom";
+import { twMerge } from "tailwind-merge";
+
+interface NavBarItemProps extends NavLinkProps {
+  caption: string
+}
+
+export default function NavBarItem({ caption, ...rest }: NavBarItemProps) {
+  return (
+    <NavLink {...rest}
+      className={({ isActive }) =>
+        twMerge("flex items-center my-2 px-4 rounded-lg hover:bg-slate-700",
+          isActive
+            ? "font-bold"
+            : "font-light")
+      }
+    >
+      <span>
+        {caption}
+      </span>
+    </NavLink>
+  )
+}
