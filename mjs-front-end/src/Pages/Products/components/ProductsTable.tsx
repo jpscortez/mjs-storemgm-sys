90f73b1a3a204ae709764ecf7e21a15294c49b77
joao cortez <jps.cortez@hotmail.com>
2024-10-19 18:16:31 -0300

    basic table visuals

--- mjs-front-end/src/Pages/Products/components/ProductsTable.tsx
+++ mjs-front-end/src/Pages/Products/components/ProductsTable.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
 
 interface ProductsTableProps {
   products: any[]
@@ -7,6 +7,25 @@ interface ProductsTableProps {
 export default function ProductsTable({ products }: ProductsTableProps) {
 
   return (
-    <></>
+    <Table>
+      <TableHeader>
+        <TableRow className="uppercase">
+          <TableHead className="w-6 text-center">Código</TableHead>
+          <TableHead>Nome</TableHead>
+          <TableHead className="w-18">Categoria</TableHead>
+          <TableHead className="w-12 text-right">Preço (R$)</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {products.map((product, i) => (
+          <TableRow key={i}>
+            <TableCell className="font-medium text-center">{product.code}</TableCell>
+            <TableCell>{product.name}</TableCell>
+            <TableCell>{product.category}</TableCell>
+            <TableCell className="text-right">{product.price.toFixed(2)}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
